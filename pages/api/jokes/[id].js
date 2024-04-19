e7bd4a8c591af870fb469d5e891ddda406e38a09
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-19 15:02:09 +0200

    initial commit

---
+++ pages/api/jokes/[id].js
@@ -0,0 +1,31 @@
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const joke = await Joke.findById(id);
+
+    if (!joke) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(joke);
+    return;
+  }
+
+  if (request.method === "PUT") {
+    const updatedJoke = request.body;
+    await Joke.findByIdAndUpdate(id, updatedJoke);
+    response.status(200).json({ status: "Joke updated!" });
+    return;
+  }
+
+  if (request.method === "DELETE") {
+    await Joke.findByIdAndDelete(id);
+    response.status(200).json({ status: "Joke deleted!" });
+    return;
+  }
+}
